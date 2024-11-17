9e5e2b2e2441a697e464aeff9a7b3f0be1268794
kaleab <kaleabzegeye1@gmail.com>
2024-11-17 14:23:29 +0300

    Implement CRUD operations for medical business data

---
+++ EthiopianMedicalDataWarehouse/crud.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_medical_business(db: Session, business: schemas.MedicalBusinessCreate):
+    db_business = models.MedicalBusiness(**business.dict())
+    db.add(db_business)
+    db.commit()
+    db.refresh(db_business)
+    return db_business
+
+def get_medical_businesses(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.MedicalBusiness).offset(skip).limit(limit).all()
